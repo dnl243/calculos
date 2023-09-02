956f059eef51ca854f9eb6d6cb69a1b33eae9dac
dnl243 <dnl243@hotmail.com.ar>
2023-09-02 01:13:23 -0300

    addesponsive

--- js/master.js
+++ js/master.js
@@ -1,27 +1,28 @@
- // Elegir 1er numero
- function getRandomNumber() {
+// Buscamos número aleatorio
+function getRandomNumber() {
   // Obtenemos un número aleatorio entre 0 y 9.
   var randomNumber = Math.floor(Math.random() * 10);
   // Devolvemos el número aleatorio.
   return randomNumber;
 }
-var randomNumber = getRandomNumber();
 
-// Elegir 2do numero
-function getRandomNumber2() {
-  // Obtenemos un número aleatorio entre 0 y 9.
-  var randomNumber2 = Math.floor(Math.random() * 10);
-  // Devolvemos el número aleatorio.
-  return randomNumber2;
-}
-var randomNumber2 = getRandomNumber2();
-
-// Elegir operacion
+// Buscamos operacion aleatoria (+,-,*)
 function chooseRandomOperation() {
   var operations = ['+', '-', '*'];
   return operations[Math.floor(Math.random() * operations.length)];
 }
+
+// Elegimos 1er número
+var randomNumber = getRandomNumber();
+console.log(randomNumber);
+
+// Elegir 2do numero
+var randomNumber2 = getRandomNumber();
+console.log(randomNumber2);
+
+// Elegimos operacion
 var operation = chooseRandomOperation();
+console.log(operation);
 
 // Captura de elementos
 var nro1 = document.getElementById('nro1');
@@ -29,8 +30,6 @@ var signo = document.getElementById('signo');
 var nro2 = document.getElementById('nro2');
 var res = document.getElementById('resultado');
 var imgResp = document.getElementById("contenedor_img");
-
-imgResp.innerHTML = '<img class="img_respuesta" id="img-res" src="./img/sonic-en-espera.webp" alt="imagen-respuesta">';
 
 // Condición nro1 >= nro2
 if (operation === '-' && randomNumber < randomNumber2) {
@@ -41,21 +40,23 @@ if (operation === '-' && randomNumber < randomNumber2) {
   signo.innerHTML = operation;
   nro2.innerHTML = randomNumber2;
 }
+imgResp.innerHTML = '<img class="img_respuesta" id="img-res" src="./img/sonic-en-espera.webp" alt="imagen-respuesta">';
+
 
 //  Realizar operacion
 function correctAnswer() {
   var result = eval(randomNumber + operation + randomNumber2);
-  return result
+  return result;
 }
 var result = eval(randomNumber + operation + randomNumber2);
-console.log(result)
+console.log(result);
 
 // Comparar result vs userAnswer
 function compareAnswers() {
   var userInputValue = document.getElementById('userAnswer').value;
   var correctAnswerValue = correctAnswer();
 
-
+  
   if (userInputValue == correctAnswerValue) {
     res.innerHTML = 'Correcto!!!';
     imgResp.innerHTML = '<img class="img_respuesta" id="img-res" src="./img/sonic-correcto.webp" alt="imagen-respuesta">';
@@ -64,6 +65,5 @@ function compareAnswers() {
     imgResp.innerHTML = '<img class="img_respuesta" id="img-res" src="./img/sonic-incorrecto.jpg" alt="imagen-respuesta">';
   }
 }
-
-const compareButton = document.getElementById('compareButton');
-compareButton.onclick = compareAnswers;
+    const compareButton = document.getElementById('compareButton');
+    compareButton.onclick = compareAnswers;
